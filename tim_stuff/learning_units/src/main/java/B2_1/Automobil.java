f4fe0b902ef06f370020176b6267d892bcee8d53
Tim Irmler <zwazel@hotmail.com>
2021-08-24 10:24:31 +0200

    done

---
+++ tim_stuff/learning_units/src/main/java/B2_1/Automobil.java
@@ -0,0 +1,19 @@
+package B2_1;
+
+public class Automobil {
+    private String automarke;
+    private String typ;
+    private int kontrollSchildNummer;
+    private String farbe;
+
+    public Automobil(String automarke, String typ, int kontrollSchildNummer, String farbe) {
+        this.automarke = automarke;
+        this.typ = typ;
+        this.kontrollSchildNummer = kontrollSchildNummer;
+        this.farbe = farbe;
+    }
+
+    public static void main(String[] args) {
+        Automobil automobil = new Automobil("Mercedes", "Auto", 12312, "Schwarz");
+    }
+}
