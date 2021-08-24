e6ecdf46e213ece51103f9f73e89c095dff16ee0
Tim Irmler <zwazel@hotmail.com>
2021-08-24 10:12:18 +0200

    auto

---
+++ tim_stuff/learning_units/src/main/java/B1_1/Main.java
@@ -0,0 +1,14 @@
+package B1_1;
+
+public class Main {
+    public Main() {
+        Auto auto1 = new Auto();
+        Auto auto2 = new Auto(23);
+        Auto auto3 = new Auto("Mercedes");
+        Auto auto4 = new Auto(200, "Audi");
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
