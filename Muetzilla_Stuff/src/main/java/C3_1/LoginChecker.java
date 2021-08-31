ccb5af3d3c8caa101ca1d07ebf56cb517751cb7a
Muetzilla <muetz-github@gasbichler.ch>
2021-08-31 16:50:11 +0200

    döner

---
+++ Muetzilla_Stuff/src/main/java/C3_1/LoginChecker.java
@@ -0,0 +1,41 @@
+package C3_1;
+
+/**
+ Beispiel f�r die Kommmunikation von 2 Klassen �ber Schnittstellen.
+
+ @Author:   bitte Namen ergänzen
+ @Date:     aktuelles Bearbeitungsdatum eintragen
+ @Version:  beginnend mit V1.0 die Versionierung nachführen
+ */
+public class LoginChecker {
+    /*
+
+    private final String user = "User";
+    private final String password = "password";
+
+    private int tryCounter = 0;
+    private final int maxtry = 5;
+
+    private LoginGUI gui;
+    public LoginChecker(LoginGUI gui){
+        this.gui = gui;
+        gui.setLoginChecker(this);
+    }
+
+    public boolean update() {
+        if (user.equals(gui.getUserName())) {
+            if (password.equals(gui.getPwd())) {
+                return true;
+            } else if (tryCounter++ <= maxtry) {
+                gui.loginAbort();
+                return false;
+            }
+        }else if (tryCounter++ >= maxtry) {
+            gui.loginAbort();
+            return false;
+        }
+        return false;
+    }
+}
+*/
+}
