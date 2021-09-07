fd417b2ae8edcb9c72ae06d548b58e69c36b5206
Muetzilla <muetz-github@gasbichler.ch>
2021-09-07 09:57:44 +0200

    Bridges

---
+++ Muetzilla_Stuff/src/main/java/D2_2/LibraryApp.java
@@ -0,0 +1,91 @@
+package D2_2;
+
+/**
+  Hier werden alle ben�tigten Objekte erstellt
+  <p>
+     <ul>
+         <li>theLibrary : Library (die Bibliothek)</li>
+         <li>max        : Librarian (der Bibliothekar)</li>
+         <li>moritz     : Customer (ein Kunde)</li>
+         <li>ursula     : Customer (eine Kundin)</li>
+     </ul>
+  </p>
+  
+  <p>Das "Drehbuch" wird gem�ss Sequenzdiagramm der Aufgabenstellung
+  implementiert.<br>
+  Dazu braucht es auch Anpassungen in den Klassen
+  <ul>
+      <li>Librarian</li>
+      <li>Customer</li>
+  </ul>
+  Die Klassen Library und Book m�ssen nicht bearbeitet werden.
+  
+  @Author:   bitte Namen erg�nzen
+  @Date:     aktuelles Bearbeitungsdatum eintragen
+  @Version:  beginnend mit V1.0 die Versionierung nachf�hren
+*/
+public class LibraryApp{
+  
+  
+  /* 
+  Hier den Ablauf implementieren
+  */
+  public static void main(String[] args){
+    // Teilauftrag 1
+    // -------------
+    // Deklaration der Hauptklassen
+    //....do it
+    
+    // Erzeugen der Objekte
+    //....do it
+    
+    
+    // -------------
+    // Teilauftrag 2
+    // -------------
+    // B�cher in die Bibliothek einreihen
+    System.out.println("Der Bibliothekar kauft neue B�cher und stellt diese in die Bibliothek");
+    //...do it
+    
+    // zur Kontrolle das Inventar der Bibliothek anzeigen
+    //....do it
+    
+    // -------------
+    // Teilauftrag 3
+    //--------------
+    // Kunden leihen ein Buch aus
+    System.out.println("\n\nEs erfolgen Ausleihen");
+    //....do it
+    
+    // zur Kontrolle das Inventar der Bibliothek anzeigen
+    //....do it
+    
+    // -------------
+    // Teilauftrag 4
+    // -------------
+    // Eine Kundin bringt das Buch selbst�ndig zur�ck.
+    System.out.println("\n\nKundin "+ursula.getName()+" bringt ein Buch zur�ck");
+    //....do it
+    
+    // zur Kontrolle das Inventar der Bibliothek anzeigen
+    //....do it
+    
+    // Ein Kunde wird gemahnt und bringt dann sein Buch zur�ck
+    System.out.println("\n\nEs folgt eine Mahnung an Kunde " + moritz.getName());
+    //....do it
+    
+    // zur Kontrolle das Inventar der Bibliothek anzeigen
+    //....do it
+    
+    // -------------
+    // Teilauftrag 5:
+    // --------------    
+    //Der Bibliothekar entfernt ein Buch aus der Bibliothek
+    System.out.println("\n\nDer Bibliothekar entfernt ein Buch aus der Bibliothek");
+    //....do it
+    
+    // zur Kontrolle das Inventar der Bibliothek anzeigen
+    //....do it
+    
+  }
+}
