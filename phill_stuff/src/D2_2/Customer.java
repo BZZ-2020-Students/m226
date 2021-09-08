4db2cb8eb2c6c60059c6a689cc22c491caf8857e
Phillip <jerebicp@bzz.ch>
2021-09-08 09:15:53 +0200

    heheehe

---
+++ phill_stuff/src/D2_2/Customer.java
@@ -0,0 +1,109 @@
+package D2_2;
+
+/**
+   Die Klasse beschreibt einen Kunden, der in der Bibliothek
+   ein Buch<br>
+   - ausleihen [ borrow_aBookByTitle() ] oder<br>
+   - zur�ckgeben [ bringBackABook() ]<br>
+   will.<br>
+   Die Ausleihe begrenzt sich pro Kunde auf ein Buch.
+   <p>Der Kunde kann gemahnt werden.</p>
+   
+   @Author:   bitte Namen erg�nzen
+   @Date:     aktuelles Bearbeitungsdatum eintragen
+   @Version:  beginnend mit V1.0 die Versionierung nachf�hren
+*/
+public class Customer{
+  
+  private String name;
+  private Book borrowedBook;
+  private Librarian theLibrarian;
+  
+  /**
+  Initialisiert ein Objekt vom Typ Kunden mit dem Namen des Kunden.
+  Zudem wird die Referenz zum Bibliothekar mitgeliefert. Diese wird dann
+  gespeichert.
+
+  */
+  public Customer(String _name, Librarian theLibrarian){
+    name = _name;
+    this.theLibrarian = theLibrarian;
+  }
+  
+  
+  /** 
+  Liefert den Namen des Kunden.
+  @return Name
+  */
+  public String getName(){
+    return name;
+  }
+  
+  
+  /**
+  Bezieht beim Bibliothekar das Buch, dessen Titel mitgegeben wird.
+  <p>Ist das Buch verf�gbar, wird die Referenz abgespeichert.<br>
+  Andernfalls (Referenz ist null) wird eine entsprechende Meldung ausgegeben.</p>
+  */
+  public void borrow_aBookByTitle(String title){
+    // Implementieren Sie entsprechend dem Sequenzdiagramm und dem
+    // hier gezeigten Struktogramm den Code.
+    //
+    //    +-----------------------------------------------------------------+
+    //    | Gib Bibliothekar den gew�nschten Buchtitel                      �
+    //    +-----------------------------------------------------------------+
+    //    � if (keine g�ltige Buchreferenez)                                �
+    //    �    --+----------------------------------------------------------+
+    //    � then � gib Meldung aus, dass Buch mit gesuchten Titel fehlt     �
+    //    �    --+----------------------------------------------------------+
+    //    � else � gib Titel und ISBN des Buchs aus, das ausgeliehen wurde  �
+    //    +------+----------------------------------------------------------+
+    //
+    //....do it
+    borrowedBook = theLibrarian.borrow_aBookToCustomerByTitle(title);
+    if (borrowedBook == null) {
+      System.out.println("Schade gibt es das Buch '"+title+"' nicht.");
+    } // end of if
+    else {
+      System.out.println(name + " leiht Buch '" + borrowedBook.getTitle() + "' aus");
+    }
+  }
+  
+  
+  /**
+  Bringt dem Bibliothekar das ausgeliehene Buch zur�ck.
+  Nach der R�ckgabe des Buchs wird die Referenz auf null gesetzt.
+  */
+  public void bringBackABook(){
+    // Implementieren Sie entsprechend dem Sequenzdiagramm den Code
+    // die lokale Referenz auf das Buch-Objekt muss "gel�scht" werden. Das...
+    // ...wird mittels Zuweisung des Wertes null bewerkstelligt!
+    //....do it
+    theLibrarian.getABookFromCustomer(borrowedBook);
+  }
+  
+  
+  /**
+  
+  */
+  public void remind(){
+    // Implementieren Sie entsprechend dem Sequenzdiagramm den Code
+    // Der Text " 'name' bringt Buch 'Titel' zur�ck" wird ausgegeben
+    //....do it
+    String titel = borrowedBook.getTitle();
+
+    System.out.println(name+" bringt Buch "+titel +" zurück");
+    bringBackABook();
+  }
+  
+  
+  /**
+  Liefert den Titel des Buchs, das durch den Kunden ausgeliehen wurde.
+  @return Buchtitel
+  */
+  public String getTitleOfBorrowedBook(){
+    // Implementieren Sie entsprechend dem Sequenzdiagramm
+    //....do it
+    return borrowedBook.getTitle();
+  }
+}
---
+++ phill_stuff/src/D2_2/Library.java
@@ -0,0 +1,130 @@
+package D2_2;
+
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Random;
+import java.util.Vector;
+
+public class Library {
+    private Vector<Book> library = new Vector();
+    private Random rand = new Random((new Date()).getTime());
+
+    public Library() {
+    }
+
+    public String addBook(Book aBook) {
+        this.library.add(aBook);
+        return this.getFreePlacement();
+    }
+
+    public void removeBook(String placement) {
+        this.removeBook(this.searchBookByPlacement(placement));
+    }
+
+    public void removeBook(Book aBook) {
+        this.library.remove(aBook);
+    }
+
+    public Book searchBookByTitle(String title) {
+        for(int i = 0; i < this.library.size(); ++i) {
+            if (((Book)this.library.get(i)).getTitle().equals(title)) {
+                return (Book)this.library.get(i);
+            }
+        }
+
+        return null;
+    }
+
+    public Book searchBookByISBN(String isbn) {
+        for(int i = 0; i < this.library.size(); ++i) {
+            if (((Book)this.library.get(i)).getISBN().equals(isbn)) {
+                return (Book)this.library.get(i);
+            }
+        }
+
+        return null;
+    }
+
+    private Book searchBookByPlacement(String placement) {
+        for(int i = 0; i < this.library.size(); ++i) {
+            if (((Book)this.library.get(i)).getPlacement().equals(placement)) {
+                return (Book)this.library.get(i);
+            }
+        }
+
+        return null;
+    }
+
+    public Book getBook(String _placement) {
+        Book b = this.searchBookByPlacement(_placement);
+        this.library.remove(b);
+        return b;
+    }
+
+    public Book getBook(Book aBook) {
+        this.library.remove(aBook);
+        return aBook;
+    }
+
+    public void putBook(Book aBook) {
+        this.library.add(aBook);
+    }
+
+    public void printInventory() {
+        Enumeration<Book> e = this.library.elements();
+
+        while(e.hasMoreElements()) {
+            Book b = e.nextElement();
+            System.out.println("-------------------------------------");
+            System.out.println("Buch:    " + b.getTitle());
+            System.out.println("ISBN:    " + b.getISBN());
+            System.out.println("Ablage:  " + b.getPlacement());
+        }
+
+    }
+
+    private String getFreePlacement() {
+        String placement = "";
+        placement = placement + (char)(this.rand.nextInt(25) + 65);
+        placement = placement + (this.rand.nextInt(30) + 10);
+        return placement;
+    }
+
+    public static void main(String[] args) {
+        Library b = new Library();
+        System.out.println("Errichte Bibliothek");
+        Book x = new Book("Ein Titel", "123-456");
+        x.setPlacement(b.addBook(x));
+        x = new Book("Ein Titel", "123-456");
+        x.setPlacement(b.addBook(x));
+        x = new Book("Ein neuer Titel", "3-223-452-898");
+        x.setPlacement(b.addBook(x));
+        x = new Book("Ein Buch", "x67-3-445-6-22");
+        x.setPlacement(b.addBook(x));
+        b.printInventory();
+        System.out.println("\nSuche Buch mit Titel 'Ein Titel'");
+        x = b.searchBookByTitle("Ein Titel");
+        if (x != null) {
+            x.print();
+        }
+
+        System.out.println("\nSuche Buch mit Titel 'falscher Titel'");
+        x = b.searchBookByTitle("falscher Titel");
+        if (x != null) {
+            x.print();
+        } else {
+            System.out.println("Buch 'falscher Titel' existiert nciht");
+        }
+
+        System.out.println("\nDas Buch 'Ein Buch' ausleihen (= aus Bibliothek nehmen)");
+        x = b.searchBookByTitle("Ein Buch");
+        b.getBook(x.getPlacement());
+        b.printInventory();
+        System.out.println("\nBuch zurückgeben");
+        b.putBook(x);
+        b.printInventory();
+        System.out.println("\nBuch 'Ein Titel' aus der Bibliothek löschen");
+        b.removeBook(b.searchBookByTitle("Ein Titel").getPlacement());
+        b.printInventory();
+    }
+}
