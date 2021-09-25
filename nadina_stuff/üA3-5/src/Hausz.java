c0d284373d6f481a245497a89180bf323c7adf25
Nadina Amsler <nadina.amsler@icloud.com>
2021-09-25 17:38:29 +0200

    übungsdruchgang
    Durchgang aller gelösten Übungen

---
+++ nadina_stuff/üA3-5/src/Hausz.java
@@ -0,0 +1,25 @@
+public class Hausz {
+
+    private int zimmer;
+    private float miete;
+
+    public Hausz(){
+
+    }
+
+    public int getZimmer() {
+        return zimmer;
+    }
+
+    public void setZimmer(int anzahl) {
+       zimmer = anzahl; // vertauscht
+    }
+
+    public float getMiete() {
+        return miete;
+    }
+
+    public void setMiete(float betrag) {
+        miete = betrag; // vertauscht
+    }
+}
