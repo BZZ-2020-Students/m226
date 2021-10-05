907321ac2e7b859df3bd6274676887765140f3cc
Muetzilla <muetz-github@gasbichler.ch>
2021-10-05 08:08:53 +0200

    LP und Lernende

--- Muetzilla_Stuff/src/main/java/uebungschulklasse/Lehrperson.java
+++ Muetzilla_Stuff/src/main/java/uebungschulklasse/Lehrperson.java
@@ -1,4 +1,16 @@
 package uebungschulklasse;
 
 public class Lehrperson {
+    private String vorname;
+    private String nachname;
+
+    public Lehrperson(String _nachname, String _vorname) {
+        nachname = _nachname;
+        vorname = _vorname;
+    }
+
+    public String getName() {
+        return vorname + " " + nachname;
+    }
+
 }
