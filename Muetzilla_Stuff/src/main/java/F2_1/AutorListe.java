16822a93eb2522abbf4284cca302b7b35a37b13f
Muetzilla <muetz-github@gasbichler.ch>
2021-10-26 14:50:27 +0200

    YES YES YES

--- Muetzilla_Stuff/src/main/java/F2_1/AutorListe.java
+++ Muetzilla_Stuff/src/main/java/F2_1/AutorListe.java
@@ -4,6 +4,24 @@ public class AutorListe {
     public static int MAX_AUTOREN;
     private Autor[] autoren = new Autor[MAX_AUTOREN];
     private int autorenCntr = 0;
-    
+
+    public AutorListe() {
+
+    }
+
+    public void addAutor(Autor autor) {
+        if (autorenCntr < MAX_AUTOREN) {
+            autoren[autorenCntr] = autor;
+            autorenCntr++;
+        }
+    }
+
+    public Autor getAutor(int index) {
+        return autoren[index];
+    }
+
+    public void printOut() {
+        
+    }
 
 }
