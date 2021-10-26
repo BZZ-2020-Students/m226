9160617b9efd94e3832099b9131c2216e0e8a5df
Phillip <jerebicp@bzz.ch>
2021-10-26 10:02:55 +0200

    f2_2 :)

--- phill_stuff/src/Zoo/Pinguin.java
+++ phill_stuff/src/Zoo/Pinguin.java
@@ -1,14 +1,17 @@
+package Zoo;
+
 /*
  * Beschreibt einen Pinguin mit der Art und dem Namen
  */
-public class Pinguin extends Tier{
+public class Pinguin extends Tier {
 
-	/**
-	 * Erzeugt ein Pinguin-Objekt und weist dem Tier einen Namen zu
-	 * @param name des Pinguins
-	 * @param wert Wert des Tiers
-	 */
-	public Pinguin(String name, float wert){
-		super("Pinguin", name, wert);
-	}
+    /**
+     * Erzeugt ein Pinguin-Objekt und weist dem Tier einen Namen zu
+     *
+     * @param name des Pinguins
+     * @param wert Wert des Tiers
+     */
+    public Pinguin(String name, float wert) {
+        super("Pinguin", name, wert);
+    }
 }
--- phill_stuff/src/Zoo/Zebra.java
+++ phill_stuff/src/Zoo/Zebra.java
@@ -1,14 +1,17 @@
+package Zoo;
+
 /*
  * Beschreibt ein Zebra mit der Art und dem Namen
  */
-public class Zebra extends Tier{
+public class Zebra extends Tier {
 
-	/**
-	 * Erzeugt ein Zebra-Objekt und weist dem Tier einen Namen zu
-	 * @param name des Zebras
-	 * @param wert Wert des Tiers
-	 */
-	public Zebra(String name, float wert){
-		super("Zebra", name, wert);
-	}
+    /**
+     * Erzeugt ein Zebra-Objekt und weist dem Tier einen Namen zu
+     *
+     * @param name des Zebras
+     * @param wert Wert des Tiers
+     */
+    public Zebra(String name, float wert) {
+        super("Zebra", name, wert);
+    }
 }
