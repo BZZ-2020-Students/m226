9e6594eb0e4515fd44aa0732a1047da91b06eda0
Nadina Amsler <nadina.amsler@icloud.com>
2021-11-01 21:51:35 +0100

    AHH I UNDERTSAND NOW 50%$

--- nadina_stuff/üE2-2/src/BlumenErbauer.java
+++ nadina_stuff/üE2-2/src/BlumenErbauer.java
@@ -1,11 +1,8 @@
 public class BlumenErbauer {
     public static void main(String[] args) {
 
-        Blume blume = new Blume();
-        blume.setName("Lilie");
-        blume.print();
-        blume = null;
-        blume.print();
-
+      Blume blume = new Blume();
+      blume.setName("Lilie");
+      blume.print();
     }
 }
