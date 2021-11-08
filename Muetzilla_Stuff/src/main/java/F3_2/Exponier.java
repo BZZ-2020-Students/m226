a3c3d3b0c73a53b0c98c183c357c1026a255dfb4
Muetzilla <muetz-github@gasbichler.ch>
2021-11-08 21:09:38 +0100

    Hatman

--- Muetzilla_Stuff/src/main/java/F3_2/Exponier.java
+++ Muetzilla_Stuff/src/main/java/F3_2/Exponier.java
@@ -7,6 +7,10 @@ public class Exponier extends MathOp {
 
     @Override
     public void execOp(float val1, float val2) {
-        
+        if(val2 < 0) {
+            throw new RuntimeException("Ungültige Operation");
+        }
+        result = (float) Math.pow(val1, val2);
+
     }
 }
