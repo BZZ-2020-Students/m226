30dcf61e548d4d183b82be85b3b7b32bf142a00c
Nadina Amsler <nadina.amsler@icloud.com>
2021-12-05 16:14:42 +0100

    demo

---
+++ nadina_stuff/collectionMap/src/MapDemo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class MapDemo {
+
+    public static void main(String[] args) {
+        Map<String, String> liste = new HashMap<>();
+        liste.put("Nadina", "Schauspieler");
+        liste.put("Moritz", "Informatiker");
+        liste.put("Thurika", "Lehrer");
+
+        System.out.println("Die Methode get()");
+        System.out.println(liste.get("Thurika"));
+
+        System.out.println("Ganze Liste");
+        System.out.println("-----------");
+        Set<String> namen = liste.keySet();
+        for (String name : namen){
+            System.out.println("Der Beruf von " + name + ": " );
+            System.out.println(liste.get(name));
+        }
+    }
+}
--- nadina_stuff/polymorphie/src/polymorphie.java
+++ nadina_stuff/polymorphie/src/polymorphie.java
@@ -7,8 +7,9 @@ public class polymorphie {
         hund dalmatiner = new dalmatiner();
         vierbeiner dalmatiner1 = new dalmatiner();
 
-        // dalmatiner 1 kann kein objekt mit der Methode rasse() haben da dieser von der Oberklasse Vierbeiner kommt
-        
+        /* dalmatiner 1 kann kein objekt mit der Methode rasse() haben da dieser den typ Vierbeiner hat
+         * und in der Oberklasse Vierbeiner gibt es nur diese Mehtode*/
+
         /* dalmatiner kann ein Objekt mit der Methode renn() und rasse()haben, das die Klasse Hund von vierbeiner erbt
         * und somit bei Methoden implementiert hat*/
 
