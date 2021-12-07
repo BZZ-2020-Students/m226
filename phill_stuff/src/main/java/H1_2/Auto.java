f402105cb0fbf2f64eea63d85e5e40709eca1f4c
Phillip <jerebicp@bzz.ch>
2021-12-07 16:46:18 +0100

    ...

--- phill_stuff/src/main/java/H1_2/Auto.java
+++ phill_stuff/src/main/java/H1_2/Auto.java
@@ -2,11 +2,19 @@ package H1_2;
 
 public class Auto {
     private String marke;
+    private String model;
+
+    private Float leistung;
+    private Float preis;
     private String kennzeichen;
 
-    public Auto(String marke, String kennzeichen) {
+    public Auto(String marke, String model, Float leistung, Float preis, String kennzeichen) {
         this.marke = marke;
+        this.model = model;
+        this.leistung = leistung;
+        this.preis = preis;
         this.kennzeichen = kennzeichen;
+
     }
 
     public String getMarke() {
@@ -16,4 +24,17 @@ public class Auto {
     public String getKennzeichen() {
         return kennzeichen;
     }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Float getLeistung() {
+        return leistung;
+    }
+
+    public Float getPreis() {
+        return preis;
+    }
+
 }
