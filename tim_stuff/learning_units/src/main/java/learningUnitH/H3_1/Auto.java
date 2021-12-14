65eade1ffc37bcf1b34d9cc2701ecf41a8a24810
Tim Irmler <zwazel@hotmail.com>
2021-12-14 15:12:20 +0100

    starting h3_1

---
+++ tim_stuff/learning_units/src/main/java/learningUnitH/H3_1/Auto.java
@@ -0,0 +1,24 @@
+package learningUnitH.H3_1;
+
+public class Auto {
+
+    private String marke;
+    private String kennzeichen;
+
+    public Auto(String marke, String kennzeichen) {
+        this.marke = marke;
+        this.kennzeichen = kennzeichen;
+    }
+
+    public String getMarke() {
+        return marke;
+    }
+
+    public String getKennzeichen() {
+        return kennzeichen;
+    }
+
+    public String toString() {
+        return (kennzeichen + " - " + marke);
+    }
+}
