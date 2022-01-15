cacfe911da199ab0eb591636926fb26fcc577ae5
Nadina Amsler <nadina.amsler@icloud.com>
2022-01-15 15:09:15 +0100

    mhm JUNit

--- nadina_stuff/üG3-1/src/ArrayDemoTest.java
+++ nadina_stuff/üG3-1/src/ArrayDemoTest.java
@@ -2,131 +2,129 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
 
 public class ArrayDemoTest {
-
     private static ArrayDemo demo;
 
-    public static void main(String[] args) {
-    }
-
+    // Methode setUp klar vordefiniert -> Folie 13 in Unit G Folie
     @Before
     public void setUp() {
         demo = new ArrayDemo();
     }
 
+    // assesrtEquals auf Folie 14 in Unit G Folie
     @Test
     public void testGroesse(){
-       assertEquals(demo.MAX_STELLEN,demo.getMaxGroesse());
+        // Methode(konstanten Wert, Parameter)
+        assertEquals(demo.MAX_STELLEN, demo.getMaxGroesse());
+    }
+
+    /* Testen der Methode getAnzahlElemente()
+    Überprüfung korreten Verhaltens ins verschiedenen Situationen*/
+    @Test
+    public void testEmptyArray(){
+        assertEquals(0, demo.getAnzahlElemente());
     }
 
     @Test
-    public void testEmpthyArray(){
-        assertEquals(0,demo.getAnzahlElemente());
-    }
-
-    @Test
-    public void testNotEmpthyArray(){
-        try {
-            demo.addWert(6f);
-        } catch (Exception e){
-            e.getMessage();
-        } finally {
-            assertEquals(1,demo.getAnzahlElemente());
-        }
+    public void testNotEmptyArray(){
+       try{
+           demo.addWert(5f);
+       }catch (Exception e){
+           // nicht behandeln
+       }finally {
+           assertEquals(1,demo.getAnzahlElemente());
+       }
     }
 
     @Test
     public void testFullArray(){
         try {
-            for (int i = 0;  i < demo.MAX_STELLEN; i++)
-                demo.addWert(4.5f);
-            }catch (Exception e){
-                e.getMessage();
-            } finally {
+            // nicht grösser als Array länge
+            for (int i = 0; i < demo.MAX_STELLEN; i++) {
+                demo.addWert(4.6f);
+            }
+        }catch (Exception e){
+            // nicht behandeln
+        }finally {
             assertEquals(demo.MAX_STELLEN, demo.getAnzahlElemente());
         }
     }
 
+    /* Testen der Methode addWert()
+     Überprüfung einer Methode, welche im Fehlerfall eine Exception wird*/
+
     @Test
     public void testAddValid(){
-        try {
-            demo.addWert(5f);
-        } catch (NotenbereichException e){
-            System.out.println("Kein Korrekter Test!");
-        }
-
-    }
-
-    @Test
-    public void testAddInvalid(){
-        try {
-            demo.addWert(13f);
-        } catch (NotenbereichException e){
-            System.out.println("Kein Korrekter Test");
+        try{
+            demo.addWert(4f);
+            // fail(); // lässt den Test durchfallen - fail wenn das erreicht wird
+        }catch(NotenbereichException e){
+            System.out.println("Kein korrekter Test!");
         }
     }
 
-    @Test
-    public void testfall1(){
-        try {
-            demo.getNote(-1);
-        } catch (Exception e){
-            e.getMessage();
-            System.out.println(e.getMessage());
-        }
+    // Wird die Exception in der Annotation angegeben und im Code nicht behandelt
+    @Test(expected = NotenbereichException.class)
+    public void testAddInvalid() throws  NotenbereichException{
+        demo.addWert(3);
+    }
+
+    // Testfälle
+
+    /* Erwartetes Ergebniss : ArrayIndexOutOfBoundsException wird geworfen
+    * geworfen mit throws also wird fie Exception in der Annotation angegeben*/
+    @Test (expected = ArrayIndexOutOfBoundsException.class)
+    public void testfall1() throws ArrayIndexOutOfBoundsException {
+        demo.getNote(-1);
     }
 
     @Test
     public void testfall2(){
-        try {
+        try{
             demo.addWert(5.0f);
-            demo.getNote(0);
-        } catch(NotenbereichException e){
-            e.getMessage();
-        }
-    }
-
-    @Test
-    public void testfall3(){
-        try {
-            demo.getNote(5);
-        } catch (Exception e){
-            e.getMessage();
-            System.out.println(e.getMessage());
-        }
-    }
-
-    @Test
-    public void removeWert1(){
-        try {
-            demo.addWert(1f);
-            demo.removeWert(-1);
-            assertEquals(1f, demo.getNote(-1));
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-    }
-
-    @Test
-    public void removeWert2(){
-        try {
-            demo.addWert(5.0f);
-            demo.removeWert(0);
+        }catch (NotenbereichException e){
+            // nicht behandeln
+            // Notenbereich exception damit die Note sich im richtigen Bereich befindet
+        }finally{
             assertEquals(5.0f, demo.getNote(0));
-        } catch (Exception e){
-            System.out.println(e.getMessage());
         }
     }
 
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testfall3() throws ArrayIndexOutOfBoundsException{
+        demo.getNote(5);
+    }
+
+    // Testen der Methode removeWert()
+    /*
+    * Test fall 1 | Eingabewert: -1 | Testwert: - | EE: ArrayIndexOutOfBoundsException wird geworfen
+    * */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void negativeIndex() throws ArrayIndexOutOfBoundsException{
+        demo.removeWert(-1);
+    }
+
+    /*
+     * Test fall 2 | Eingabewert: 7 | Testwert: - | EE: ArrayIndexOutOfBoundsException wird geworfen
+     * */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void highIndex() throws ArrayIndexOutOfBoundsException{
+        demo.removeWert(7);
+    }
+
+    /*
+     * Test fall 3 | Eingabewert: 0 | Testwert: 5.0f | EE: --
+     * */
+
     @Test
-    public void removeWert3(){
-        try {
-            demo.addWert(4f);
-            demo.removeWert(5);
-            assertEquals(4f, demo.getNote(5));
-        } catch (Exception e){
-            System.out.println(e.getMessage());
+    public void removeValidE(){
+        try{
+            demo.addWert(5.0f);
+        }catch (NotenbereichException e){
+            e.getMessage();
         }
+        demo.removeWert(0);
     }
 }
