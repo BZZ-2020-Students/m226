a67cd53b2fb5671cf62dc124c1801e1c3354bba8
Nadina Amsler <nadina.amsler@icloud.com>
2022-01-17 12:16:46 +0100

    3-1

--- nadina_stuff/üH3-1/src/Auto.java
+++ nadina_stuff/üH3-1/src/Auto.java
@@ -1,22 +1,34 @@
 public class Auto  implements Comparable<Auto>{
     private String marke;
     private String kennzeichen;
+    private String farbe;
 
-    public Auto(String marke, String kennzeichen){
+    public Auto(String marke, String kennzeichen, String farbe){
         this.marke = marke;
         this.kennzeichen = kennzeichen;
+        this.farbe = farbe;
     }
+
 
     public String getMarke() {
         return marke;
     }
 
     public String getKennzeichen() {
-        return (kennzeichen +  " - " + marke);
+        return (kennzeichen +  " - " + marke + " " + farbe);
+    }
+
+    public String getFarbe(){
+        return farbe;
     }
 
     @Override
     public int compareTo(Auto other) {
         return kennzeichen.compareTo(other.getKennzeichen());
+    }
+
+    @Override
+    public String toString() {
+        return marke + "| "  + kennzeichen + "| " + farbe;
     }
 }
