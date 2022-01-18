78e1b05ab3f6c2f941c0d2e26d4860f865d7337a
Zwazel <zwazel@hotmail.com>
2022-01-18 14:21:08 +0100

    init frage 3

---
+++ tim_stuff/lb02_m226b/src/main/java/aufgabe3/SpielerErzeugungTest.java
@@ -0,0 +1,9 @@
+package aufgabe3;
+
+
+public class SpielerErzeugungTest {
+
+	// Hier folgen die Implementationen für die Testfälle 1 - 6
+	// Es muss in jedem Fall der Konstruktor ausgeführt werden, da unterschiedliche Parameter überprüft werden müssen
+	
+}
