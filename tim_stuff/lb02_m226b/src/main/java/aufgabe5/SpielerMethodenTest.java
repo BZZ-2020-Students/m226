a94cf76442dedf4cfbfb935b522075365775f9ae
Zwazel <zwazel@hotmail.com>
2022-01-18 14:44:03 +0100

    init frage 5

---
+++ tim_stuff/lb02_m226b/src/main/java/aufgabe5/SpielerMethodenTest.java
@@ -0,0 +1,14 @@
+package aufgabe5;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class SpielerMethodenTest {
+    // Deklaration einer Referenzvariable für die Nutzung in den einzelnen Testfällen
+  
+    @BeforeEach
+    //gültiges Objekt für die Test erzeugen
+    public void add() {
+    }
+
+    // Hier folgen die Implementationen für die Testfälle 7 - 11
+}
